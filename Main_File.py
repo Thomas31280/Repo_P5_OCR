a25bfb327e7dc27016a344586397a1942848d22b
Thomas Duval <tomas1905@hotmail.fr>
2021-03-04 14:39:19 +0100

    First step : Data recovery, DB & TB création

--- Main_File.py
+++ Main_File.py
@@ -10,10 +10,28 @@ print(test_var) #Check the response of API ( ok if 200 )
 """
 Generating a database from the JSON data.
 """
-# Connexion to server :
+# Connexion to MySQL :
 cnx = SQLcmd.connect(user='root', password='edgard31280', host='localhost')
-cnx.close()
+cursor = cnx.cursor()
 
 # Database creation
+DB_NAME = "db_test"
+
+cursor.execute("DROP DATABASE IF EXISTS {}".format(DB_NAME))
+cursor.execute("CREATE DATABASE {} DEFAULT CHARACTER SET 'utf8'".format(DB_NAME))
+print("Database {} created successfully.".format(DB_NAME))
+cnx.database = DB_NAME
 
 # First test on database (table creation) with connector :
+TB_NAME = "table_test"
+
+cursor.execute(
+    "CREATE TABLE `table_test` ("
+    "  `id` SMALLINT NOT NULL AUTO_INCREMENT,"
+    "  `Field_1` date NOT NULL,"
+    "  `Field_2` varchar(14) NOT NULL,"
+    "  `Field_3` varchar(16) NOT NULL,"
+    "  `Field_4` enum('M','F') NOT NULL,"
+    "  `Field_5` date NOT NULL,"
+    "  PRIMARY KEY (`id`)"
+    ") ENGINE=InnoDB")

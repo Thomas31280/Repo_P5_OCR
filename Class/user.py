a983b4e051de6a19497b7382a371c5866137cf5c
Thomas Duval <tomas1905@hotmail.fr>
2021-04-06 17:26:43 +0200

    Fix bugs, clear screen, rebuilt user interface

--- Class/user.py
+++ Class/user.py
@@ -1,33 +1,40 @@
 import mysql.connector as SQLcmd
+import os
 
 import constant_storage
+
+if os.name == 'nt':
+    clear = lambda: os.system('cls')
+else:
+    clear = lambda: os.system('clear')
 
 class User:
 
     def __init__(self):
-        
-        self.first_try = True
-
+        pass
 
     def user_main_page (self, cursor, connexion):
-        
-        user_answer = input("Tapez 1 si vous souhaitez remplacer un aliment, 2 si vous souhaitez retrouver vos aliments substitués. Sinon, tapez Q pour quitter le programme : ")
+
+        clear()
+        user_answer = input("1 - Quel aliment souhaitez-vous remplacer ?\n2 - Retrouver mes aliments substitués.\nQ - Quitter le programme\n")
         
         if user_answer == "Q":
             print("Merci, et à bientôt")
 
         elif user_answer == "1":
             
+            clear()
             cursor.execute("SELECT * FROM category ORDER BY id_category")
             myresult = cursor.fetchall()
             
             for category in myresult:
                 print(category)
             
-            user_choice = input("Veuillez choisir une catégorie en entrant le chiffre correspondant, puis appuyez sur entrée : ")
+            user_choice = input("\nVeuillez choisir une catégorie en entrant le chiffre correspondant, puis appuyez sur entrée : ")
 
-            if int(user_choice) in range(1, len(constant_storage.CATEGORIES )+1):
-                self.products_by_category(user_choice, cursor, connexion)
+            if user_choice.isdigit():
+                if int(user_choice) in range(1, len(constant_storage.CATEGORIES )+1):
+                    self.products_by_category(user_choice, cursor, connexion)
             
             else:
                 print("Entrée non valide.")
@@ -35,6 +42,8 @@ class User:
 
         elif user_answer == "2":
             
+            clear()
+            print("Voici la liste de vos produits sauvegardés :\n")
             query = ("SELECT * FROM favorites")
             cursor.execute(query)
             myresult = cursor.fetchall()
@@ -48,6 +57,7 @@ class User:
                 user_choice_5 = input("Si vous souhaitez poursuivre, tapez 1, sinon, tapez Q pour quitter le programme : ")
 
                 if user_choice_5 == "Q":
+                    legit_request = True
                     print("Merci, et à bientôt")
                 
                 elif user_choice_5 == "1":
@@ -62,7 +72,8 @@ class User:
     
 
     def products_by_category (self, user_choice, cursor, connexion):
-        
+
+        clear()
         id_products_returned = []
         
         query = ("SELECT id_product, product_name FROM product INNER JOIN category ON product.categories = category.category_name WHERE category.id_category = %s")
@@ -74,9 +85,12 @@ class User:
             id_products_returned.append(product[0])
             print(product)
         
-        user_choice_2 = input("Veuillez choisir un produit parmi ceux proposés, en entrant son numéro. Sinon, quittez le programme avec Q : ")
+        user_choice_2 = input("\nVeuillez choisir un produit parmi ceux proposés, en entrant son numéro. Sinon, quittez le programme avec Q : ")
 
-        if int(user_choice_2) in id_products_returned:
+        if str(user_choice_2) == "Q":
+            print("Merci, et à bientôt")
+        
+        elif int(user_choice_2) in id_products_returned:
             query = ("SELECT nutriscore FROM product WHERE product.id_product = %s")
             answer = (int(user_choice_2), )
             cursor.execute(query, answer)
@@ -85,11 +99,7 @@ class User:
             for result in myresult:
                 nutriscore_product = result[0]
             
-            self.substitute_proposal(user_choice, nutriscore_product, cursor, connexion)
-
-        elif user_choice_2 == "Q":
-            print("Merci, et à bientôt")  ###PROBLEM TO SOLVE HERE###
-            
+            self.substitute_proposal(user_choice, nutriscore_product, cursor, connexion)    
         
         else:
             print("Entrée non valide.")
@@ -98,19 +108,17 @@ class User:
     
     def substitute_proposal (self, user_choice, nutriscore_product, cursor, connexion):
 
+        clear()
         id_products_returned = []
-        
-
-        ###PROBLEM TO SOLVE HERE###
 
         if nutriscore_product == None:
-            print("Le produit que vous avez sélectionné ne possède pas d'information liée à son nutriscore. Par défaut, nous vous proposons les produits de sa catégorie pour lesquels aucune information de nutriscore existe :")
-            query = ("SELECT id_product, product_name, stores, url FROM product INNER JOIN category ON product.categories = category.category_name WHERE category.id_category = %s AND product.nutriscore = NULL")
+            print("\nLe produit que vous avez sélectionné ne possède pas d'information liée à son nutriscore. Par défaut, nous vous proposons les produits de sa catégorie pour lesquels aucune information de nutriscore existe :\n")
+            query = ("SELECT id_product, product_name, stores, url FROM product INNER JOIN category ON product.categories = category.category_name WHERE category.id_category = %s AND product.nutriscore IS NULL")
             answer = (int(user_choice), )
 
         else:
             answer = (int(user_choice), nutriscore_product)
-            query = ("SELECT id_product, product_name, stores, url FROM product INNER JOIN category ON product.categories = category.category_name WHERE category.id_category = %s AND product.nutriscore = %s")
+            query = ("SELECT id_product, product_name, stores, url FROM product INNER JOIN category ON product.categories = category.category_name WHERE category.id_category = %s AND product.nutriscore <= %s")
 
         cursor.execute(query, answer)
         myresult = cursor.fetchall()
@@ -119,39 +127,49 @@ class User:
             id_products_returned.append(product[0])
             print(product)
         
-        user_choice_3 = input("Si vous souhaitez ajouter un produit à vos favoris, veuillez choisir entrer son numéro. Sinon, appruyez sur Q pour quitter le programme : ")
+        user_choice_3 = input("\nSi vous souhaitez ajouter un produit à vos favoris, veuillez choisir entrer son numéro. Sinon, appruyez sur Q pour quitter le programme : ")
 
-        if int(user_choice_3) in id_products_returned:
-            self.add_product_in_favorites(user_choice_3, cursor, connexion)
+        if user_choice_3.isdigit():
+            if int(user_choice_3) in id_products_returned:
+                self.add_product_in_favorites(user_choice_3, cursor, connexion)
+            else:
+                print("\nEntrée non valide")
+                self.substitute_proposal(user_choice, nutriscore_product, cursor, connexion)
 
         elif user_choice_3 == "Q":
             print("Merci, et à bientôt")
         
         else:
-            print("Entrée non valide")
+            print("\nEntrée non valide")
             self.substitute_proposal(user_choice, nutriscore_product, cursor, connexion)
         
     
     def add_product_in_favorites (self, product_id, cursor, connexion):
         
+        clear()
         neutral_cursor = cursor
         legit_request = False
         
         query = ("INSERT INTO favorites (id_product,url,categories,product_name,nutriscore,stores) SELECT id_product,url,categories,product_name,nutriscore,stores FROM product WHERE product.id_product = %s")
         id_product = (int(product_id), )
-        cursor.execute(query, id_product)
-        connexion.commit()
+        
+        try:
+            cursor.execute(query, id_product)
+            connexion.commit()
+            print("Produit ajouté aux favoris")
+        except SQLcmd.Error:
+            print("\nUne erreur est survenue, le produit est déjà dans vos favoris")
+            pass
 
         while legit_request == False:
-            user_choice_4 = input("Si vous souhaitez poursuivre, tapez 1, sinon, tapez Q pour quitter le programme : ")
-            if int(user_choice_4) == 1:
+            user_choice_4 = input("\nSi vous souhaitez poursuivre, tapez 1, sinon, tapez Q pour quitter le programme : ")
+            if user_choice_4 == '1':
                 self.user_main_page(neutral_cursor, connexion)
                 legit_request = True
             
-            elif user_choice_4 == "Q":                   ###PROBLEM TO SOLVE HERE###
+            elif user_choice_4 == "Q":
                 print("Merci, et à bientôt")
                 legit_request = True
             
             else:
                 print("Entrée non valide")
-            
